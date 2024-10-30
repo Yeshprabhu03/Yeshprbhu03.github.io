e08cd2a0a5463f9901fd3ef659aaba441ab4e2f9
Yeshprabhu03 <yd20@fordham.edu>
2024-10-30 18:10:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// JavaScript for smooth scrolling
+// Smooth scrolling for all anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
